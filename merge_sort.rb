8d1b0ed298a64a8f84dfa8e029c3da47bcee5e63
Stefan L. Barrieres <slb5418@gmail.com>
2016-08-17 18:20:02 -0400

    refactored merge_sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,40 +2,20 @@ def merge_sort(arr)
 	## basecase - return the array if the length is <= 1
 	return arr if arr.length <= 1
 
-	mid = arr.length/2
-	left = arr[0...mid]
-	right = arr[mid..-1]
-
 	# recursive case - continue diving arrays into two parts
-	left = merge_sort(left)
-	right = merge_sort(right)
+	mid = arr.length/2
+	left = merge_sort(arr[0...mid])
+	right = merge_sort(arr[mid..-1])
 
-	# finally, merged the sorted lists
-	return merge(left, right)
-end
-
-def merge(left, right)
-
+	# merge
 	result = []
 
-	while not left.empty? and not right.empty?
+	until left.empty? || right.empty?
 		left.first < right.first ? result << left.shift : result << right.shift
 	end
-
-	if not left.empty?
-		while not left.empty?
-			result << left.shift
-		end
-	end
-
-	if not right.empty?
-		while not right.empty?
-			result << right.shift
-		end
-	end	
-
+	
+	result += left + right
 	return result
-
 end
 
 p merge_sort([80249,25,354,46537,96,987,3465,8,3245,876,78,5465,987,46,876,9])
